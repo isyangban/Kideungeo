51719d5599767770c35d0ff1ae0ce9c3b2530b56
Jeong Woon Choi <choijeongwoon@gmail.com>
2019-09-28 21:23:55 +0900

    Remove unsed comments

--- internal/kideungeo/dc_con_test.go
+++ internal/kideungeo/dc_con_test.go
@@ -6,7 +6,6 @@ import (
 
 func TestSearchDCCon(t *testing.T) {
 	dcCons := searchDCCon("붕괴")
-	// t.Log("dcCons: ", dcCons)
 
 	if len(dcCons) == 0 {
 		t.Error("Search Result should not be empty")
